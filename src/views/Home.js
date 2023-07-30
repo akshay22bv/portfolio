add6dbe9ef99e8e389f4eef0a5b8ffe6812c50c1
akshay22bv <akshay22bv@gmail.com>
2023-07-30 11:11:47 +0530

    cloth e-commerce project added

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-slate-100">
       <div className="bg-slate-100 m-auto px-[15%] sm:px-[20%] flex flex-col justify-center w-full h-full">
-        <p className="text-red-600 py-1">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl ">Akshay Bilagi</h1>
+        <p className="py-1 text-red-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl">Akshay Bilagi</h1>
         <h1 className="text-4xl sm:text-6xl text-slate-700">
           I,m a Full Stack Developer
         </h1>
@@ -19,9 +19,9 @@ const Home = () => {
 
         <div className="">
           <Link to="skills" smooth={true} duration={500}>
-            <button className="flex items-center group px-3 py-3 bg-red-500 text-white hover:bg-red-600 rounded">
+            <button className="flex items-center px-3 py-3 text-white bg-red-500 rounded group hover:bg-red-600">
               View Skills
-              <span className="ml-3 group-hover:rotate-90 duration-500">
+              <span className="ml-3 duration-500 group-hover:rotate-90">
                 <HiArrowNarrowRight size={30} />
               </span>
             </button>
--- src/views/Works.js
+++ src/views/Works.js
@@ -6,8 +6,8 @@ const Works = () => {
     <div name="works" className="w-full">
       <div className="bg-slate-100 flex flex-col px-[15%] py-[10%] items-center w-full h-full">
         <div className=" max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className="md:text-right pb-8">
-            <p className="text-2xl sm:text-4xl font-vol inline font-semibold border-b-4 border-red-500">
+          <div className="pb-8 md:text-right">
+            <p className="inline text-2xl font-semibold border-b-4 border-red-500 sm:text-4xl font-vol">
               {"Works"}
             </p>
           </div>
@@ -16,13 +16,13 @@ const Works = () => {
         <div className="max-w-[1000px] w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
           {data?.map((item, i) => (
             <a target="_blank" href={item?.url} key={i} rel="noreferrer">
-              <div className="flex flex-col items-center justify-center drop-shadow cursor-pointer w-full">
+              <div className="flex flex-col items-center justify-center w-full cursor-pointer drop-shadow">
                 <img
                   alt=""
                   src={item.image}
-                  className="h-40 w-full rounded-lg object-fill hover:scale-105 duration-300"
+                  className="object-fill w-full h-40 duration-300 rounded-lg hover:scale-105"
                 />
-                <h1 className=" font-bold text-xl py-2 text-center">
+                <h1 className="py-2 text-xl font-bold text-center ">
                   {item.title}
                 </h1>
               </div>
